da49002d9dafb0789b9f152c07c19ad923f7f225
Harrison <hborges005@gmail.com>
2018-05-22 16:44:59 -0600

    fix weird file issue

--- Ratings/PlayerDetailsViewController.swift
+++ Ratings/PlayerDetailsViewController.swift
@@ -8,6 +8,23 @@
 
 import UIKit
 
-class PlayerDetailsViewController: UICollectionViewController {
+class PlayerDetailsViewController: UITableViewController {
+  @IBOutlet weak var nameTextField: UITextField!
+  @IBOutlet weak var detailLabel: UILabel!
   
+  var player: Player?
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == "SavePlayerDetail", let playerName = nameTextField.text {
+      player = Player(name: playerName, game: "New Game", rating: 2)
+    }
+  }
+}
+
+extension PlayerDetailsViewController {
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    if indexPath.section == 0 {
+      nameTextField.becomeFirstResponder()
+    }
+  }
 }
